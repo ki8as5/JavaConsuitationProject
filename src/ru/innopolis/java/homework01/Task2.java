84b5253f951f60d126fbe6c49bdcbdae013a61f7
Magin Kirill <magin.kirill666@gmail.com>
2023-11-09 16:34:01 +0300

    add homework01

---
+++ src/ru/innopolis/java/homework01/Task2.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int petya = rand.nextInt(0,3);
+        int vasya = rand.nextInt(0,3);
+        if (petya == 0) {
+            if (vasya == 0) {
+                System.out.println("Ничья");
+            } else if (vasya == 1) {
+                System.out.println("petya");
+            } else if (vasya == 2) {
+                System.out.println("vasya");
+        } }else if (petya == 1){
+            if (vasya == 0) {
+                System.out.println("vasya");
+            } else if (vasya == 1) {
+                System.out.println("Ничья");
+            } else if (vasya == 2) {
+                System.out.println("petya");
+            }
+        } else if (petya == 2) {
+            if (vasya == 0) {
+                System.out.println("petya");
+            } else if (vasya == 1) {
+                System.out.println("vasya");
+            } else if (vasya == 2) {
+                System.out.println("Ничья");
+            }
+        }
+
+    }
+}
+
