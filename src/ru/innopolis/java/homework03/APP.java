c6efe617618baebee74deb314b47d0b0988aac18
Magin Kirill <magin.kirill666@gmail.com>
2023-11-09 16:35:39 +0300

    add homework03

---
+++ src/ru/innopolis/java/homework03/APP.java
@@ -0,0 +1,14 @@
+package ru.innopolis.java.homework03;
+public class APP {
+    public static void main(String[] args) {
+        TV sony = new TV();
+        System.out.println(sony.toString());
+        TV samsung = new TV( "samsung" , 100.7 , "Neo QLED 8K QN900C" , "white"   );
+        System.out.println(samsung.toString());
+
+
+
+
+
+    }
+}
---
+++ src/ru/innopolis/java/homework03/TV.java
@@ -0,0 +1,74 @@
+package ru.innopolis.java.homework03;
+
+import java.util.Scanner;
+
+public class TV {
+    private String Brand ;
+    private double Diagonal;
+    private String Color;
+    private String Model;
+    Scanner go = new Scanner(System.in);
+
+    public TV (){
+        this.Brand = go.nextLine();
+        this.Diagonal = go.nextDouble();
+        go.nextLine();
+        this.Model = go.nextLine();
+        this.Color = go.nextLine();
+
+
+
+    }
+
+    public TV (
+            String brand,
+            double Diagonal,
+            String Model,
+            String Color
+        ){
+        this.Brand = brand;
+        this.Diagonal = Diagonal;
+        this.Model = Model;
+        this.Color = Color;
+    }
+    public String getBrand() {
+        return Brand;
+    }
+
+    public void setBrand(String brand) {
+        Brand = brand;
+    }
+
+    public double getDiagonal() {
+        return Diagonal;
+    }
+
+    public void setDiagonal(double diagonal) {
+        Diagonal = diagonal;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+    public String toString(){
+        return String.format("TV с параметрами; \n brand: %s\n Diagonal: %s\n Model: %s\n Color: %s" ,
+                this.Brand,
+                this.Diagonal,
+                this.Model,
+                this.Color);
+    }
+
+
+}
